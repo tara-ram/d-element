6d74540e9792ba61eb3a17b02b1d7d97e4204d6a
Kate_Rz <54590715+tara-ram@users.noreply.github.com>
2020-04-21 11:21:44 +0400

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#team-btn').on('click', function () {
+        $('.hidden').show();
+    })
+    $('#form-btn').on('click', function () {
+        $('.hidden').hide();
+    })
+})
