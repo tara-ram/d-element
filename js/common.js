37fa5a917512c45e4b4b21fa4ccdc59c15bfce9a
Kate_Rz <54590715+tara-ram@users.noreply.github.com>
2020-04-21 12:06:11 +0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#team-btn').on('click', function () {
         $('.hidden').show();
-    });
+    })
     $('#form-btn').on('click', function () {
         $('.hidden').hide();
     })
