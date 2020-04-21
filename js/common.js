37d06eacfb4568fc0f0a663aabfa3c37893202eb
Kate_Rz <54590715+tara-ram@users.noreply.github.com>
2020-04-21 21:42:20 +0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
     $('#team-btn').on('click', function () {
         $('.hidden').show();
-    })
-    $('#form-btn').on('click', function () {
-        $('.hidden').hide();
-    })
-})
+    });
+    $('#form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url:'',
+            method: 'POST',
+            data: $('form').serialize(),
+            success: function () {
+                $('.popup').show();
+            }
+
+        });
+        
+    });
+});
